a1c1b52fc4dd0c0af5bce4ab6c2e46bf825919b6
Ivan <racioglobus@yandex.ru>
2023-07-02 22:12:09 +0300

    add small fix to all project

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
 
+// edit popup
+
 let popup = document.querySelector('.popup');
 let profile = document.querySelector('.profile');
-
 let popupClose = popup.querySelector('.popup__close-button');
 let popupOpen = profile.querySelector('.profile__edit-button');
 
 function openPopup() {
-  // добавить класс элементу
   popup.classList.add('popup_opened');
 }
 
 function closedPopup() {
-  // удалить класс элемента
   popup.classList.remove('popup_opened');
 }
 
