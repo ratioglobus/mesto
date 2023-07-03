36f2c6a7d39c9a12ba7e9e8a32ce1e055cdfbe54
Ivan <racioglobus@yandex.ru>
2023-07-03 22:34:03 +0300

    add edit popup to profile and small fix html/css

--- js/index.js
+++ js/index.js
@@ -1,18 +1,36 @@
 
-// edit popup
-
 let popup = document.querySelector('.popup');
 let profile = document.querySelector('.profile');
-let popupClose = popup.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__form');
+
 let popupOpen = profile.querySelector('.profile__edit-button');
+let popupClose = popup.querySelector('.popup__close-button');
+let popupSaveButton = popup.querySelector('.popup__save-button');
+
+let valueProfileName = profile.querySelector('.profile__name');
+let valueProfileAbout = profile.querySelector('.profile__about');
+let valuePopupName = popup.querySelector('.popup__input-value_name');
+let valuePopupAbout = popup.querySelector('.popup__input-value_about');
+
 
 function openPopup() {
   popup.classList.add('popup_opened');
+  valuePopupName.value = valueProfileName.textContent;
+  valuePopupAbout.value = valueProfileAbout.textContent;
 }
 
 function closedPopup() {
   popup.classList.remove('popup_opened');
 }
 
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    valueProfileName.textContent = valuePopupName.value;
+    valueProfileAbout.textContent = valuePopupAbout.value;
+    popupSaveButton.addEventListener('click', closedPopup);
+}
+
+popupSaveButton.addEventListener('click', closedPopup);
 popupOpen.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closedPopup);
+formElement.addEventListener('submit', handleFormSubmit);
