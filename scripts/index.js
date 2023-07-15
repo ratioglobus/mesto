4e83d2d243f8ce47b6b7c5d9a97d5833dc7eb82d
Ivan <racioglobus@yandex.ru>
2023-07-15 03:29:04 +0300

    start 5 sprint: add 2 popup and js code

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,54 @@
 
 let popup = document.querySelector('.popup');
-let profile = document.querySelector('.profile');
 let formElement = document.querySelector('.popup__form');
-
-let popupOpen = profile.querySelector('.profile__edit-button');
 let popupClose = popup.querySelector('.popup__close-button');
 let popupSaveButton = popup.querySelector('.popup__save-button');
 
+let profile = document.querySelector('.profile');
+let popupOpen = profile.querySelector('.profile__edit-button');
 let valueProfileName = profile.querySelector('.profile__name');
 let valueProfileAbout = profile.querySelector('.profile__about');
 let valuePopupName = popup.querySelector('.popup__input_value_name');
 let valuePopupAbout = popup.querySelector('.popup__input_value_about');
+
+const initialCards = [
+  {
+    name: 'Хогвартс',
+    link: './images/hogwarts.jpg'
+  },
+  {
+    name: 'Ривенделл',
+    link: './images/rivendal.jpg'
+  },
+  {
+    name: 'Галактический город',
+    link: './images/galactik-city.jpg'
+  },
+  {
+    name: 'Новиград',
+    link: './images/novigrad.jpg'
+  },
+  {
+    name: 'Вайтран',
+    link: './images/whiterun.jpg'
+  },
+  {
+    name: 'Штормград',
+    link: './images/schtormgrad.jpg'
+  }
+];
+
+
+initialCards.forEach(function ({ name, link }) {
+  const elementsList = document.querySelector('.elements');
+  const elementsTemplate = document.querySelector('#template-elements').content;
+  const elementsItem = elementsTemplate.cloneNode(true);
+
+  elementsItem.querySelector('.elements__photo').src = link;
+  elementsItem.querySelector('.elements__place').textContent = name;
+
+  elementsList.append(elementsItem);
+});
 
 
 function openPopup() {
