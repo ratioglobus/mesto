91f61986aa7740335c3bfece0c395d5448db965a
Ivan <racioglobus@yandex.ru>
2023-07-17 16:07:41 +0300

    refactoring js code, add image popup and small fix

--- scripts/const.js
+++ scripts/const.js
@@ -1,40 +1,46 @@
 
-// значения формы, профиля и фото
+// форма редактирования профиля
 const profile = document.querySelector('.profile');
 const formProfile = document.querySelector('.popup-profile__form');
-const formAddImage = document.querySelector('.popup-addimage__form');
-
-const elementsItem = document.querySelector('.elements__item');
-
-// значения попапа редактирования профиля
 const popupProfile = document.querySelector('.popup-profile');
 const popupProfileOpen = profile.querySelector('.profile__edit-button');
 const popupProfileClose = popupProfile.querySelector('.popup__close-button');
 const popupSaveButton = popupProfile.querySelector('.popup__save-button');
 
-// тексты попапа редактирования профиля
+// значения попапа редактирования профиля
 const valuePopupName = popupProfile.querySelector('.popup__input_value_name');
 const valuePopupAbout = popupProfile.querySelector('.popup__input_value_about');
 const valueProfileName = profile.querySelector('.profile__name');
 const valueProfileAbout = profile.querySelector('.profile__about');
 
-// значения и тексты попапа добавления фотографий
-const valuePopupAddImageName = document.querySelector('.popup__input_value_name-addimage');
-const valuePopupAddImageLink = document.querySelector('.popup__input_value_link-addimage');
+
+// template блок
+const elements = document.querySelector('.elements');
+const elementsItem = document.querySelector('.elements__item');
+const elementsTemplate = document.querySelector('#template-elements');
+
+
+// форма добавления фотографий
 const popupAddImage = document.querySelector('.popup-addimage');
-const popupAddImageOpen = document.querySelector('.profile__add-button');
-const popupAddImageClose = document.querySelector('.popup-addimage__close-button');
+const formAddImage = document.querySelector('.popup-addimage__form');
 const formPopupAddImage = document.querySelector('.popup-addimage__save-button');
 
 
-// const openImagePopupElement = elementsItem.querySelector('.elements__photo');
-// const imagePopupAbout = document.querySelector('.popup-image__about');
-// const imagePopupImage = document.querySelector('.popup-image__img');
-// const popupImages = document.querySelector('.popup-image');
+// значения попапа добавления фотографий
+const valuePopupAddImageName = document.querySelector('.popup__input_value_name-addimage');
+const valuePopupAddImageLink = document.querySelector('.popup__input_value_link-addimage');
+const popupAddImageOpen = document.querySelector('.profile__add-button');
+const popupAddImageClose = document.querySelector('.popup-addimage__close-button');
 
 
+// попап раскрытия фото
+const popupImages = document.querySelector('.popup-image');
+const imagePopupAbout = document.querySelector('.popup-image__about');
+const imagePopupImage = document.querySelector('.popup-image__img');
+const popupImagesClose = document.querySelector('.popup-image__close-button');
 
-// массив объектов фотографий, загружаемых на страницу
+
+// массив объектов фотографий, загружаемых изначально на страницу
 const initialCards = [
   {
     name: 'Хогвартс',
--- scripts/index.js
+++ scripts/index.js
@@ -1,107 +1,113 @@
 
-// открыть и закрыть попап редактирования профиля
-function openClosedPopupProfile() {
-  popupProfile.classList.toggle('popup_opened');
+// открыть и закрыть попап
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+};
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+};
+
+
+// открыть попап профиля и отправить данные через форму
+function openPopupProfile() {
   valuePopupName.value = valueProfileName.textContent;
   valuePopupAbout.value = valueProfileAbout.textContent;
+  openPopup(popupProfile);
 };
 
-
-// отправить данные в форму редактирования профиля
 function editProfile (evt) {
-    evt.preventDefault();
-    valueProfileName.textContent = valuePopupName.value;
-    valueProfileAbout.textContent = valuePopupAbout.value;
-    openClosedPopupProfile();
-};
-
-
-// открыть и закрыть попап добавления новых фотографий
-function openClosedPopupAddImage() {
-  popupAddImage.classList.toggle('popup_opened');
-  valuePopupAddImageName.value = '';
-  valuePopupAddImageLink.value = '';
-};
-
-
-// загрузить первые фотографии на страницу из массива
-initialCards.forEach(function ({ name, link }) {
-  const elementsList = document.querySelector('.elements');
-  const elementsTemplate = document.querySelector('#template-elements').content;
-  const elementsItem = elementsTemplate.cloneNode(true);
-
-  elementsItem.querySelector('.elements__photo').src = link;
-  elementsItem.querySelector('.elements__place').textContent = name;
-
-  elementsList.append(elementsItem);
-});
-
-
-// добавить новую фотографию
-function createNewImage (evt) {
   evt.preventDefault();
-  const elementsTemplate = document.querySelector('#template-elements').content;
-  const elementsList = document.querySelector('.elements');
-  const elementsItem = elementsTemplate.cloneNode(true);
-
-  elementsItem.querySelector('.elements__place').textContent = valuePopupAddImageName.value;
-  elementsItem.querySelector('.elements__photo').src = valuePopupAddImageLink.value;
-
-  elementsList.prepend(elementsItem);
-  popupAddImage.classList.toggle('popup_opened');
-
-  // поставить лайк новым фотографиям
-  const likeButtons = document.querySelector('.elements__like-button');
-  likeButtons.addEventListener('click', function (evt) {
-    const eventTarget = evt.target;
-    eventTarget.classList.toggle('elements__like-button_active');
-  });
-
-  // удалить новую фотографию
-  const deleteButton = document.querySelector('.elements__delete');
-  deleteButton.addEventListener('click', function () {
-    const listItem = deleteButton.closest('.elements__item');
-    listItem.remove();
-  });
+  valueProfileName.textContent = valuePopupName.value;
+  valueProfileAbout.textContent = valuePopupAbout.value;
+  closePopup(popupProfile);
 };
 
 
-// поставить лайк всем остальным фотографиям
-const likeButtons = Array.from(document.querySelectorAll('.elements__like-button'));
+// открыть попап добавления фотографии
+function openPopupElements() {
+  formAddImage.reset();
+  openPopup(popupAddImage);
+};
 
-likeButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    button.classList.toggle('elements__like-button_active');
+
+// создать новый элемент с возможностью лайкнуть его и открыть
+function createElement({ name, link }) {
+  const cloneElement = elementsTemplate.content.cloneNode(true);
+  const elementsItem = cloneElement.querySelector('.elements__item');
+
+  elementsItem.querySelector('.elements__place').textContent = name;
+  elementsItem.querySelector('.elements__photo').src = link;
+  elementsItem.querySelector('.elements__photo').alt = name;
+
+  const likeElementButton = elementsItem.querySelector('.elements__like-button');
+  likeElementButton.addEventListener('click', function (event) {
+    event.target.classList.toggle('elements__like-button_active');
   });
+
+  const openImagePopupElement = elementsItem.querySelector('.elements__photo');
+  openImagePopupElement.addEventListener('click', function () {
+    imagePopupAbout.textContent = name;
+    imagePopupImage.src = link;
+    imagePopupImage.alt = name;
+    openPopup(popupImages);
+  });
+
+  return elementsItem;
+};
+
+
+// загрузить из массива на страницу первые фото, используя шаблон createElement
+initialCards.forEach(item => {
+  const elementsItem = createElement(item);
+  elements.append(elementsItem);
 });
 
 
-// удалить загруженную фотографию из общего списка
-const deleteButtons = Array.from(document.querySelectorAll('.elements__delete'));
+// добавить и удалить новый элемент
+function addNewElement(evt) {
+  evt.preventDefault();
+  const name = valuePopupAddImageName.value;
+  const link = valuePopupAddImageLink.value;
+  const elementsItem = createElement({ name, link });
 
-deleteButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const listItem = button.closest('.elements__item');
-    listItem.remove();
+  elements.prepend(elementsItem);
+
+  const deleteButtons = Array.from(document.querySelectorAll('.elements__delete'));
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const listItem = button.closest('.elements__item');
+      listItem.remove();
+    });
+  });
+
+  closePopup(popupAddImage);
+};
+
+
+// удалить уже добавленный элемент
+const deleteButtons = Array.from(document.querySelectorAll('.elements__delete'));
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const listItem = button.closest('.elements__item');
+      listItem.remove();
   });
 });
 
 
 // слушатели событий
-popupProfileOpen.addEventListener('click', openClosedPopupProfile);
-popupProfileClose.addEventListener('click', openClosedPopupProfile);
+
+// закрыть каждый из попапов
+popupProfileClose.addEventListener('click', () => closePopup(popupProfile));
+popupAddImageClose.addEventListener('click', () => closePopup(popupAddImage));
+popupImagesClose.addEventListener('click', () => closePopup(popupImages));
+
+// открыть попап с формой редактирования профиля
+popupProfileOpen.addEventListener('click', openPopupProfile);
 formProfile.addEventListener('submit', editProfile);
 
-popupAddImageOpen.addEventListener('click', openClosedPopupAddImage);
-popupAddImageClose.addEventListener('click', openClosedPopupAddImage);
-formAddImage.addEventListener('submit', createNewImage);
+// открыть попап с формой добавления фото
+popupAddImageOpen.addEventListener('click', openPopupElements);
 
-
-
-// открыть попап с фотографией
-
-
-
-
-// openPopupImage.addEventListener('click', openClosedPopupImage);
-// closePopupImageBtn.addEventListener('click', openClosedPopupImage);
+// добавить новый элемент
+formAddImage.addEventListener('submit', addNewElement);
